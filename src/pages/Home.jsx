dc578b420fe55a44bbe55530323ef7f923361c78
Serhii Neliubov <sergeyneliubov@gmail.com>
2023-08-06 12:01:54 +0300

    add pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,15 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import PizzaBlock from "../components/PizzaBlock";
+import ReactPaginate from "react-paginate";
+import Pagination from "../components/Pagination";
+import Search from "../components/Search";
 
 const Home = ({ searchValue }) => {
   let [pizzas, setPizzas] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [sort, setSort] = React.useState({
     name: "популярности ↓",
     sortProperty: "rating",
@@ -16,13 +20,13 @@ const Home = ({ searchValue }) => {
   const order = sort.sortProperty.includes("-") ? "asc" : "desc";
   const sortBy = sort.sortProperty.replace("-", "");
   const category = categoryId > 0 ? `category=${categoryId}` : "";
-  const search = searchValue > 0 ? `&search=${searchValue}` : "";
+  const search = searchValue ? `&search=${searchValue}` : "";
 
   React.useEffect(() => {
     setIsLoading(true);
 
     fetch(
-      `https://64ce5ff00c01d81da3eec0ab.mockapi.io/pizzas?${category}&sortBy=${sortBy}&order=${order}${search}`
+      `https://64ce5ff00c01d81da3eec0ab.mockapi.io/pizzas?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((response) => response.json())
       .then((data) => {
@@ -30,9 +34,10 @@ const Home = ({ searchValue }) => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sort, searchValue]);
+  }, [categoryId, sort, searchValue, currentPage]);
 
   const items = pizzas.map((pizza) => <PizzaBlock {...pizza} key={pizza.id} />);
+
   const skeletons = [...new Array(6)].map((_, index) => (
     <Skeleton key={index} />
   ));
@@ -48,6 +53,7 @@ const Home = ({ searchValue }) => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeletons : items}</div>
+      <Pagination onPageChange={(value) => setCurrentPage(value)} />
     </div>
   );
 };
